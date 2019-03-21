b015224893764ef0f122a2cdcf9b39b53e39d298
我不会搬砖啊 <1427963351@qq.com>
2019-03-21 22:18:28 +0800

    Update main.py

--- myweb_local/app/Views/main.py
+++ myweb_local/app/Views/main.py
@@ -5,11 +5,3 @@ main = Blueprint('main',__name__)
 @main.route('/',methods = ['GET','POST'])
 def homepage():
     return render_template('homepages/homepage.html')
-
-@main.route('/blog/<num>')
-def bloginfoshow(num=None):
-    return 'hello'
-
-@main.route('/blog')
-def blogsshow():
-    return 'hello'
