7db23c3148578758f0734fba35fd075846de90c2
我不会搬砖啊 <1427963351@qq.com>
2019-03-22 18:44:08 +0800

    Update BlogMethods.py

--- myweb_local/app/AssistMethod/BlogMethods.py
+++ myweb_local/app/AssistMethod/BlogMethods.py
@@ -2,6 +2,11 @@
 # BlogMethods 用于处理保存博客，显示博客，保存记录，删除记录等问题
 ###########################################################
 
-def readblog(path):
-    return
+SOURCE_PACKAGE = 'app/Data/index.json'
+
+class Blog_index:
+    def __init__(self):
+        return
+
+
 
