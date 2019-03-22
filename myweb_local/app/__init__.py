1f8f0f06812c7ef17eefddfe90693c0016e83856
我不会搬砖啊 <1427963351@qq.com>
2019-03-22 18:55:17 +0800

    Update __init__.py

--- myweb_local/app/__init__.py
+++ myweb_local/app/__init__.py
@@ -1,5 +1,24 @@
 from flask import Flask,render_template
 from app.Views import config_blueprint
+from flask_login import login_user, login_required
+from flask_login import LoginManager, current_user
+from flask_login import login_user
+from flask_wtf import CsrfProtect
+from flask_session import Session
+from app.AssistMethod.UserMethods import User
+import os
+
+def config_login(app):
+    app.secret_key = os.urandom(24)
+    login_manager = LoginManager()
+    login_manager.session_protection = "strong"
+    login_manager.login_view = 'LoginIn'
+    login_manager.init_app(app)
+    # app.config['SESSION_TYPE'] = 'filesystem'
+    # Session(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
 
 def config_errorhandler(app):
     @app.errorhandler(404)
@@ -10,6 +29,5 @@ def create_app():
     app = Flask(__name__)
     config_blueprint(app)
     config_errorhandler(app)
+    config_login(app)
     return app
-
-
