38bad07e215f5ae51a399d125abaf5dffd3ebc60
我不会搬砖啊 <1427963351@qq.com>
2019-03-22 18:53:14 +0800

    Update Adminstrator.py

--- myweb_local/app/Views/Adminstrator.py
+++ myweb_local/app/Views/Adminstrator.py
@@ -1,18 +1,18 @@
 from flask import Blueprint,render_template,url_for,request,redirect
-from app.AssistMethod.UserMethods import checkUserAndPassword
-from .main import homepage
+from app.AssistMethod.UserMethods import User
+from flask_login import login_user,current_user
+
 adminstartor = Blueprint('admin',__name__)
 
 @adminstartor.route('/',methods = ['GET','POST'])
-def homepage():
+def login():
     if request.method == 'POST':
         usrname = request.form['username']
         password = request.form['password']
-        result = checkUserAndPassword(usrname,password)
-        if result is True:
-            # return render_template('homepages/homepage.html')
+        user = User(usrname)
+        if user.verify_password(password):
+            login_user(user,remember=False)
             return redirect(url_for('main.homepage'))
         else:
-            return render_template('adminstartor/LoginIn.html',warn = '用户名或密码错误')
+            return render_template('adminstartor/LoginIn.html', warn='用户名或密码错误')
     return render_template('adminstartor/LoginIn.html')
-
