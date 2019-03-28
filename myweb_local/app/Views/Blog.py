ddb6c6eada59be4124e60084121d2864d139b964
我不会搬砖啊 <1427963351@qq.com>
2019-03-28 21:40:33 +0800

    Update Blog.py

--- myweb_local/app/Views/Blog.py
+++ myweb_local/app/Views/Blog.py
@@ -1,12 +1,49 @@
-from flask import Blueprint,render_template,url_for
+from flask import Blueprint,render_template,url_for,request
+from flask_login import current_user
+from app.AssistMethod.BlogMethods import Blog
 
 blog = Blueprint('blog',__name__)
 
-@blog.route('/',methods = ['GET','POST'])
+
+@blog.route('/')
 def multiple_blogs():
-    return render_template('homepages/blog_multiply.html')
+    try:
+        name = current_user.username
+    except:
+        name = None
+    if name is not None:
+        return render_template('blog/blog_multiply.html', user=name)
+    return render_template('blog/blog_multiply.html')
+
 
 @blog.route('/<num>')
 def bloginfoshow(num=None):
     print(num)
     return 'hello'+str(num)
+
+
+@blog.route('/write',methods = ['GET','POST'])
+def write_blog():
+    if request.method == "POST":
+        blog = Blog()
+        img = request.files['file']
+        title = request.form['title']
+        introduce = request.form['introduce']
+        content = request.form['content']
+        if "::private" in title:
+            title = title.replace("::private",'')
+            public = False
+        else:
+            public = True
+        print(public)
+        print(title)
+        print(introduce)
+        print(content)
+        # img.save("app/static/images/text.png")
+        # img.save(url_for("static",filename="images/1.png"))
+    # return render_template('blog/blog_write.html',user=current_user.username)
+    return render_template('blog/blog_write.html')
+
+@blog.route('/<num>/modify')
+def modify(num):
+    return str(num)+"modify"
