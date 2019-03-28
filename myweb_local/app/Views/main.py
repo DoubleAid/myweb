564fbd39c556bd82db5583c7963f58a7f1b65499
我不会搬砖啊 <1427963351@qq.com>
2019-03-28 21:41:05 +0800

    Update main.py

--- myweb_local/app/Views/main.py
+++ myweb_local/app/Views/main.py
@@ -1,20 +1,19 @@
-from flask import Blueprint,render_template,url_for,redirect
-from flask_login import current_user,logout_user
-main = Blueprint('main',__name__)
+from flask import Blueprint, render_template, url_for, redirect
+from flask_login import current_user, logout_user
+main = Blueprint('main', __name__)
 
-@main.route('/',methods = ['GET','POST'])
+
+@main.route('/', methods=['GET','POST'])
 def homepage():
     try:
         name = current_user.username
     except:
         name = None
     if name is not None:
-        return render_template('homepages/homepage.html',cuser = name)
+        return render_template('homepages/homepage.html', user=name)
     else:
         return render_template('homepages/homepage.html')
-@main.route('/blog/<num>')
-def bloginfoshow(num=None):
-    return 'hello'
+
 
 @main.route('/Logout')
 def logout():
