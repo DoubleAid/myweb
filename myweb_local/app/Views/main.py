f96ee3908ba3f53d9b321c4c0cc7faf22b56d48b
我不会搬砖啊 <1427963351@qq.com>
2019-04-04 17:28:00 +0800

    Update main.py

--- myweb_local/app/Views/main.py
+++ myweb_local/app/Views/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect,request, jsonify
 from flask_login import current_user, logout_user
+from app.AssistMethod.BlogMethods import Blog
 main = Blueprint('main', __name__)
 
 
@@ -14,6 +15,16 @@ def homepage():
     else:
         return render_template('homepages/homepage.html')
 
+@main.route('/getnext')
+def get_next_date():
+    id = request.args.get('page', 0, type=int)
+    blog = Blog(uuid=id, type="num")
+    if blog.get_blog_num() < id:
+        return jsonify(result={'page':id, 'html':None})
+    else:
+        data = blog.get_data()
+        page = id+1
+        return jsonify(result={'page':page, 'html':data})
 
 @main.route('/Logout')
 def logout():
