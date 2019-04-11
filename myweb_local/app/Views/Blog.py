bda34bbfa5521ca14adb36d0e23f13f3727134cc
我不会搬砖啊 <1427963351@qq.com>
2019-04-11 20:38:01 +0800

    Update Blog.py

--- myweb_local/app/Views/Blog.py
+++ myweb_local/app/Views/Blog.py
@@ -9,7 +9,11 @@ blog = Blueprint('blog',__name__)
 @blog.route('/')
 def multiple_blogs():
     blog = Blog(uuid=1,type="num")
-    data = blog.get_data()
+    print(blog.get_blog_num())
+    if blog.get_blog_num() < 1:
+        data = None
+    else:
+        data = blog.get_data()
     try:
         name = current_user.username
     except:
@@ -29,19 +33,20 @@ def bloginfoshow(num=None):
     if permission is False:
         try:
             name = current_user.username
-            return render_template('blog/blog_single.html',user=name ,data=data)
+            return render_template('blog/blog_single.html',user=name, data=data)
         except:
             return 404
     else:
         return render_template('blog/blog_single.html', data=data)
 
-@blog.route('/page<num>')
-def blogs_show_by_page(num=None):
-    if num == 1:
-        return
 
 @blog.route('/write',methods = ['GET','POST'])
 def write_blog():
+    try:
+        name = current_user.username
+    except:
+        return redirect(url_for('admin.login'))
+    print("test1")
     if request.method == "POST":
         blog = Blog()
         title = request.form['title']
@@ -66,15 +71,57 @@ def write_blog():
         blog.add_introduce(introduce)
         blog.add_content(content)
         blog.save()
+        print("test2")
         return redirect(url_for('blog.bloginfoshow',num = blog.id))
         # img.save("app/static/images/text.png")
         # img.save(url_for("static",filename="images/1.png"))
     # return render_template('blog/blog_write.html',user=current_user.username)
-    return render_template('blog/blog_write.html')
+    return render_template('blog/blog_write.html', user=name)
 
-@blog.route('/<num>/modify')
+
+@blog.route('/<num>/modify',methods=['GET', 'POST'])
 def modify(num):
-    # blog = Blog(num)
-    return str(num)+"modify"
+    blog = Blog(num)
+    try:
+        name = current_user.username
+    except:
+        return redirect(url_for('admin.login'))
+    if request.method == "POST":
+        title = request.form['title']
+        introduce = request.form['introduce']
+        try:
+            files = request.files.getlist('file')
+            if len(files) != 0:
+                blog.delete_images()
+                for file in files:
+                    blog.add_image(file)
+        except:
+            pass
+        # file_path = CURRENT_PATH+"/app/Data/Blog/"+str(blog.id)+"/1."
+        # file.save("D://a.jpg")
+        content = request.form['content']
+        if len(title) == 0 or len(introduce) == 0 or len(content) == 0:
+            return 404
+        if "::private" in title:
+            title = title.replace("::private", '')
+            blog.set_permission(flag=False)
+        else:
+            blog.set_permission(flag=True)
+        blog.add_title(title)
+        blog.add_introduce(introduce)
+        blog.add_content(content)
+        blog.save()
+        return redirect(url_for('blog.bloginfoshow', num=blog.id))
+        # img.save("app/static/images/text
+    else:
+        data = blog.get_data()
+        return render_template('blog/blog_write.html', user=name, data=data)
 
-
+@blog.route('/<num>/delete')
+def delete(num):
+    try:
+        name = current_user.username
+    except:
+        return redirect(url_for('admin.login'))
+    Blog.delete_blog(num)
+    return redirect(url_for('blog.multiple_blogs'))
