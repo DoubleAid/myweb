7cf9799091f20962c1fd24fa9f928cb28347f7bd
我不会搬砖啊 <1427963351@qq.com>
2019-12-16 17:36:34 +0800

    Update Blog.py

--- myweb_local/app/Views/navigation/Blog.py
+++ myweb_local/app/Views/navigation/Blog.py
@@ -77,3 +77,51 @@ def write_blog():
         # img.save(url_for("static",filename="images/1.png"))
     # return render_template('blog/blog_write.html',user=current_user.username)
     return render_template('blog/blog_write.html', user=name)
+
+
+@blog.route('/<num>/modify',methods=['GET', 'POST'])
+def modify(num):
+    blog = Blog(num)
+    try:
+        name = current_user.username
+    except:
+        return redirect(url_for('admin.login'))
+    if request.method == "POST":
+        title = request.form['title']
+        introduce = request.form['introduce']
+        try:
+            files = request.files.getlist('file')
+            if len(files) != 0:
+                blog.delete_images()
+                for file in files:
+                    blog.add_image(file)
+        except:
+            pass
+        # file_path = CURRENT_PATH+"/app/Data/Blog/"+str(blog.id)+"/1."
+        # file.save("D://a.jpg")
+        content = request.form['content']
+        if len(title) == 0 or len(introduce) == 0 or len(content) == 0:
+            return 404
+        if "::private" in title:
+            title = title.replace("::private", '')
+            blog.set_permission(flag=False)
+        else:
+            blog.set_permission(flag=True)
+        blog.add_title(title)
+        blog.add_introduce(introduce)
+        blog.add_content(content)
+        blog.save()
+        return redirect(url_for('blog.bloginfoshow', num=blog.id))
+        # img.save("app/static/images/text
+    else:
+        data = blog.get_data()
+        return render_template('blog/blog_write.html', user=name, data=data)
+
+@blog.route('/<num>/delete')
+def delete(num):
+    try:
+        name = current_user.username
+    except:
+        return redirect(url_for('admin.login'))
+    Blog.delete_blog(num)
+    return redirect(url_for('blog.multiple_blogs'))
