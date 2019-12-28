5bcfa042068db2c880cf88d05bd8d71f17e27fda
我啊个呸啊 <1427963351@qq.com>
2019-12-28 19:11:21 +0800

    Update listener_action.js

--- myweb_local/app/static/js/blog/blog_write/js/listener_action.js
+++ myweb_local/app/static/js/blog/blog_write/js/listener_action.js
@@ -35,9 +35,13 @@ $('div[class="CodeMirror-code"]').keydown(enter_key_down);
 
 function enter_key_down(event){
     if(event.keyCode ==13){
-        var article_list = [];
+        // 禁止默认行为
+        event.cancelBubble=true;
+        event.preventDefault();
+        event.stopPropagation();
+        // 判断光标是不是选中了内容
         var selection = window.getSelection();
-        if(selection.rangeCount > 0){
+        if(selection.rangeCount > 1){
             console.log("有多处段落被选中，先不处理");
         }
         var range = selection.getRangeAt(0);
@@ -45,38 +49,35 @@ function enter_key_down(event){
             console.log("重合");
             console.log(range.startOffset);
         }
-        content = range.startContainer;
-        console.log(content.length);
-        
+        content = range.startContainer.textContent;
         container = range.commonAncestorContainer;
+        // 光标处 会将 源字符串分为两部分
+        var old_line_word = content.substring(0,range.startOffset);
+        var new_line_word = content.substring(range.startOffset,content.length);
+        // 获取 pre 元素
         var pre_element = $(container).parents("pre.CodeMirror-line");
+        // 获取 回车时 光标所在的行数
         line_num = Number(pre_element.prev().text());
-        console.log(line_num);
-        pres = $('div[class="CodeMirror-code"]').find("pre");
-        for (i=1;i<= pres.length;i++){
-            console.log($(pres[i-1]).text());
-            article_list.push($(pres[i-1]).text())
+        // 获取所有的行
+        divs = $('div[class="CodeMirror-code"]').children();
+        // 如果被修改的行数为第一行，需要特殊处理
+        if(line_num == 1){
+            $(divs[line_num-1]).find(".cm-header").html(old_line_word);
+        }else{
+            $(divs[line_num-1]).find("span").html(old_line_word);
         }
-        $('div[class="CodeMirror-code"]').find("pre").each(function(){
-            console.log($(this).text());
-            article_list.push($(this).text());
-        });
-        console.log(article_list);
-        var strn = article_list.join("\n");
-        console.log('pre 获取地字符串为：'+ strn );
-        console.log("当前textarea的值为");
-        console.log($('textarea[name="value"]').val());
-        $('textarea[name="value"]').val(strn);
-        console.log("修改值之后");
-        console.log($('textarea[name="value"]').val());
-        $('textarea[name="value"]').val("123");
-        console.log("修改值123之后");
-        $('textarea[name="value"]').val();
-        CodeMirror_lines = ""
-        for(i=1;i<article_list.length;i++){
-            CodeMirror_lines += build_CodeMirror_line(article_list[i-1],i);
+        for (i=line_num;i<divs.length;i++){
+            $(divs[i]).find(".CodeMirror-linenumber").html(i+2);
         }
-        $("div[class='CodeMirror-code']").html(123+CodeMirror_lines)
+        $(divs[line_num-1]).after(build_CodeMirror_line(new_line_word,line_num+1));
+        selection.removeAllRanges();
+        var new_divs = $('div[class="CodeMirror-code"]').children();
+        ele = ($(new_divs[line_num]).find("span"))[0];
+        
+        var range = document.createRange()
+        range.setStart(($(new_divs[line_num]).find("span"))[0],0);
+        range.setEnd(($(new_divs[line_num]).find("span"))[0],0);
+        selection.addRange(range);
     }
 }
 
@@ -84,9 +85,9 @@ function build_CodeMirror_line(word,line){
     console.log(word);
     ans = '<div style="position: relative;">'+
             '<div class="CodeMirror-gutter-wrapper" contenteditable="false" style="left: -53px;">'+
-            '<div class="CodeMirror-linenumber CodeMirror-gutter-elt" style="left: 0px; width: 21px;">'+i+
-            '</div></div><pre class=" CodeMirror-line " role="presentation"><span role="presentation" style="padding-right: 0.1px;"><span class="cm-header cm-header-1">'+
-            word +'</div>';
+            '<div class="CodeMirror-linenumber CodeMirror-gutter-elt" style="left: 0px; width: 21px;">'+line+
+            '</div></div><pre class=" CodeMirror-line " role="presentation"><span role="presentation" style="padding-right: 0.1px;">'+
+            word +'</span></pre></div>';
     console.log("ans为"+ans);
     return ans
 }
