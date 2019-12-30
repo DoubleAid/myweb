dba98907fa090352790e45f342f8a3a7de9dd28c
我啊个呸啊 <1427963351@qq.com>
2019-12-30 17:28:36 +0800

    Update listener_action.js

--- myweb_local/app/static/js/blog/blog_write/js/listener_action.js
+++ myweb_local/app/static/js/blog/blog_write/js/listener_action.js
@@ -1,93 +1,98 @@
 function addLoadEvent(func) {
     var oldonload = window.onload;
-    if (typeof window.onload != 'function'){
+    if (typeof window.onload !== "function"){
         window.onload = func;
     }else{
         window.onload=function(){
             oldonload();
             func();
-        }
+        };
     }
 }
 
-function t1() {
-    change_button_color();
-}
-
-addLoadEvent(t1);
-
-/* edit and preview 按钮相关监听事件 */
-function change_button_color(){
-    $('button[class="selected"]').css({
-        "border": "2px solid red",
-    });
-}
-//$(function(){
-//    $('div[class="CodeMirror-code"]').children().on('keydown',function(event){
-//        if (event.keyCode == 13) {
-//            console.log(event.target);
-//            $(event.target).parent().html();
-//        }
-//    });
-//})
-
+// 文章内容按键事件响应
 $('div[class="CodeMirror-code"]').keydown(enter_key_down);
 
 function enter_key_down(event){
-    if(event.keyCode ==13){
+    // 判断光标是不是选中了内容
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+    // 光标所在的容器
+    var content = range.startContainer.textContent;
+    var container = range.commonAncestorContainer;
+    // 光标处 会将 源字符串分为两部分
+    var beforeWords = content.substring(0,range.startOffset);
+    var afterWords = content.substring(range.startOffset,content.length);
+    // 行数
+    var preElement = $(container).parents("pre.CodeMirror-line");
+    var lineNum = Number(preElement.prev().text());
+    // 获取所有的行
+    divs = $('div[class="CodeMirror-code"]').children();
+    // 点击回车键
+    if(event.keyCode === 13){
         // 禁止默认行为
         event.cancelBubble=true;
         event.preventDefault();
         event.stopPropagation();
-        // 判断光标是不是选中了内容
-        var selection = window.getSelection();
-        if(selection.rangeCount > 1){
-            console.log("有多处段落被选中，先不处理");
-        }
-        var range = selection.getRangeAt(0);
-        if(range.collapsed){
-            console.log("重合");
-            console.log(range.startOffset);
-        }
-        content = range.startContainer.textContent;
-        container = range.commonAncestorContainer;
-        // 光标处 会将 源字符串分为两部分
-        var old_line_word = content.substring(0,range.startOffset);
-        var new_line_word = content.substring(range.startOffset,content.length);
-        // 获取 pre 元素
-        var pre_element = $(container).parents("pre.CodeMirror-line");
-        // 获取 回车时 光标所在的行数
-        line_num = Number(pre_element.prev().text());
-        // 获取所有的行
-        divs = $('div[class="CodeMirror-code"]').children();
         // 如果被修改的行数为第一行，需要特殊处理
-        if(line_num == 1){
-            $(divs[line_num-1]).find(".cm-header").html(old_line_word);
+        console.log(beforeWords);
+        console.log(selection.rangeCount);
+        if(lineNum === 1){
+            $(divs[lineNum-1]).find(".cm-header").text(beforeWords);
         }else{
-            $(divs[line_num-1]).find("span").html(old_line_word);
+            $(divs[lineNum-1]).find("span").text(beforeWords);
         }
-        for (i=line_num;i<divs.length;i++){
-            $(divs[i]).find(".CodeMirror-linenumber").html(i+2);
+        newLine = $(divs[lineNum-1]).clone(true);
+        newLine.find("span").text(afterWords);
+        newLine.find(".CodeMirror-linenumber").text(lineNum+1);
+        for (let i=lineNum;i<divs.length;i++){
+            $(divs[i]).find(".CodeMirror-linenumber").text(i+2);
         }
-        $(divs[line_num-1]).after(build_CodeMirror_line(new_line_word,line_num+1));
-        selection.removeAllRanges();
-        var new_divs = $('div[class="CodeMirror-code"]').children();
-        ele = ($(new_divs[line_num]).find("span"))[0];
-        
-        var range = document.createRange()
-        range.setStart(($(new_divs[line_num]).find("span"))[0],0);
-        range.setEnd(($(new_divs[line_num]).find("span"))[0],0);
-        selection.addRange(range);
-    }
-}
+        $(divs[lineNum-1]).after(newLine);
 
-function build_CodeMirror_line(word,line){
-    console.log(word);
-    ans = '<div style="position: relative;">'+
-            '<div class="CodeMirror-gutter-wrapper" contenteditable="false" style="left: -53px;">'+
-            '<div class="CodeMirror-linenumber CodeMirror-gutter-elt" style="left: 0px; width: 21px;">'+line+
-            '</div></div><pre class=" CodeMirror-line " role="presentation"><span role="presentation" style="padding-right: 0.1px;">'+
-            word +'</span></pre></div>';
-    console.log("ans为"+ans);
-    return ans
+        selection.removeAllRanges();
+        var newDivs = $('div[class="CodeMirror-code"]').children();
+        var newRange = document.createRange();
+        console.log(($(newDivs[lineNum]).find("span"))[0]);
+        newRange.setStart(($(newDivs[lineNum]).find("span"))[0],0);
+        newRange.setEnd(($(newDivs[lineNum]).find("span"))[0],0);
+        selection.addRange(newRange);
+    }
+    else if (event.keyCode === 8)
+    {
+        if (beforeWords.length === 0){
+            event.cancelBubble=true;
+            event.preventDefault();
+            event.stopPropagation();
+            if (lineNum > 1){
+                console.log("修改删除后的行数 从第"+lineNum+"开始");
+                // 将 afterwords 添加到上一行
+                //// 获取前一段的内容，注意第一行特别处理
+                var prevDiv = divs[lineNum-2];
+                if(lineNum==2){
+                    Textspan = $(prevDiv).find(".cm-header");
+                }else{
+                    Textspan = $(prevDiv).find("span");
+                }
+                startOffset = $(Textspan).text().length;
+                word = $(Textspan).text() + afterWords;
+                $(Textspan).text(word);
+                console.log($(Textspan)[0]);
+                // 确定光标位置
+                var newDivs = $('div[class="CodeMirror-code"]').children();
+                selection.removeAllRanges();
+                var newRange = document.createRange();
+                newRange.setStart(($(newDivs[lineNum-2]).find("span"))[0],0);
+                newRange.setEnd(($(newDivs[lineNum-2]).find("span"))[0],0);
+                selection.addRange(newRange);
+                // 修改下面的行数
+                for (i=lineNum;i<divs.length;i++){
+                    $(divs[i]).find(".CodeMirror-linenumber").text(i);
+                }
+                // 删除目标行
+                $(divs[lineNum-1]).remove()
+
+            }
+        }
+    }
 }
