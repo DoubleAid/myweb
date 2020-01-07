12998f9832ae14d0f58872fc6139b85437670735
我啊个呸啊 <1427963351@qq.com>
2020-01-07 10:43:48 +0800

    Update listener_action.js

--- myweb_local/app/static/js/blog/blog_write/js/listener_action.js
+++ myweb_local/app/static/js/blog/blog_write/js/listener_action.js
@@ -23,8 +23,6 @@ function enter_key_down(event){
     // 光标处 会将 源字符串分为两部分
     var beforeWords = content.substring(0,range.startOffset);
     var afterWords = content.substring(range.startOffset,content.length);
-    console.log("beforewords:"+beforeWords);
-    console.log("afterwords:"+afterWords);
     // 行数
     var preElement = $(container).parents("pre.CodeMirror-line");
     var lineNum = Number(preElement.prev().text());
@@ -38,8 +36,6 @@ function enter_key_down(event){
         // 获取所有的行
         let divs = $("div.CodeMirror-code").children();
         if(lineNum == 1){
-            console.log("lineNum is 1");
-            console.log(divs[0])
             $(divs[0]).find("span.cm-header").text(beforeWords);
         }else{
             $(divs[lineNum-1]).find("span").text(beforeWords);
@@ -50,24 +46,22 @@ function enter_key_down(event){
         }
         $(divs[lineNum-1]).after(newLine);
 
-        newDivs = $('div[class="CodeMirror-code"]').children();
+        newDivs = $('div.CodeMirror-code').children();
         newRange = document.createRange();
-        newRange.setStart(($(newDivs[lineNum]).find("span"))[0],0);
-        newRange.setEnd(($(newDivs[lineNum]).find("span"))[0],0);
+        newRange.setStart($($(newDivs[lineNum]).find("span"))[0],0);
+        newRange.setEnd($($(newDivs[lineNum]).find("span"))[0],0);
         selection.removeAllRanges();
         selection.addRange(newRange);
     }
     else if (event.keyCode === 8)
     {
-        console.log("delete button down");
-        console.log("beforewords:"+beforeWords);
-        console.log("beforewords length:"+beforeWords.length);
+        // 获取所有的行
+        let divs = $("div.CodeMirror-code").children();
         if (beforeWords.length == 0){
             event.cancelBubble=true;
             event.preventDefault();
             event.stopPropagation();
             if (lineNum > 1){
-
                 console.log("修改删除后的行数 从第"+lineNum+"开始");
                 // 将 afterwords 添加到上一行
                 //// 获取前一段的内容，注意第一行特别处理
@@ -80,13 +74,12 @@ function enter_key_down(event){
                 startOffset = $(Textspan).text().length;
                 word = $(Textspan).text() + afterWords;
                 $(Textspan).text(word);
-                console.log($(Textspan)[0]);
                 // 确定光标位置
                 var newDivs = $('div[class="CodeMirror-code"]').children();
                 selection.removeAllRanges();
                 var newRange = document.createRange();
-                newRange.setStart(($(newDivs[lineNum-2]).find("span"))[0],0);
-                newRange.setEnd(($(newDivs[lineNum-2]).find("span"))[0],0);
+                newRange.setStart(($(newDivs[lineNum-2]).find("span"))[0],1);
+                newRange.setEnd(($(newDivs[lineNum-2]).find("span"))[0],1);
                 selection.addRange(newRange);
                 // 修改下面的行数
                 for (i=lineNum;i<divs.length;i++){
@@ -102,12 +95,12 @@ function enter_key_down(event){
             event.cancelBubble=true;
             event.preventDefault();
             event.stopPropagation();
-            $(range.startContainer).text(afterWords);
-            console.log(afterWords);
+            span = $(container).parent();
+            $(span).text(afterWords);
             selection.removeAllRanges();
             var newRange = document.createRange();
-            newRange.setStart(container,0);
-            newRange.setEnd(container,0);
+            newRange.setStart(span[0],0);
+            newRange.setEnd(span[0],0);
             selection.addRange(newRange);
         }
     }
@@ -115,7 +108,6 @@ function enter_key_down(event){
 
 // 内容上传
 $("#submit").click(function () {
-    console.log("click");
     let id = $("input#blog-id").val();
     let permission = $("input.magic-checkbox").prop("checked")?1:0;
     send_data = {
@@ -124,7 +116,6 @@ $("#submit").click(function () {
         "introduce":$("textarea#introduce-textarea").val(),
         "article":get_article_content()
     };
-    console.log(send_data)
     $.ajax({
             url:'/blog/modify/'+id,
             data:send_data,
@@ -160,7 +151,7 @@ function set_article_content() {
     let article_content = $("textarea.file-editor-textarea").text();
     let line_list = article_content.split("\n");
     if(line_list.length !== 0){
-        $("pre.CodeMirror-line").find("span").text(line_list[0])
+        $("span.cm-header").text(line_list[0])
     }
     if(line_list.length > 1) {
         for (let i = 2; i <= line_list.length; i++) {
