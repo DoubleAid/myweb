8e7ace8d44d86dd8666f483563ba6ef4090b19ba
我啊个呸啊 <1427963351@qq.com>
2020-01-08 15:12:32 +0800

    Update Blog.py
    1. 添加 /blog/get_all_assort 路由，用来获取类别下拉框选项
    2. modify 函数 写数据添加验证
    3. 删除部分打印

--- myweb_local/app/Views/navigation/Blog.py
+++ myweb_local/app/Views/navigation/Blog.py
@@ -16,7 +16,6 @@ blog = Blueprint('blog',__name__)
 # ----------------- 显示微博 ---------
 @blog.route('/')
 def multiple_blogs():
-    print("multiple_blogs")
     # blog 集
     blog_set = []
     # 比较 默认显示 的 数量
@@ -46,13 +45,13 @@ def show_blog_by_uuid(num):
     if not blog_info:
         return 404
     # 获取 blog content 格式
+    print(blog_info['content']['article'])
     blog_info['content']['article'] = markdown.markdown(blog_info['content']['article'], output_format='html5')
     return render_template('blog/blog_single.html',user=user_name, blog=blog_info)
 
 @blog.route('/modify/<num>', methods=['GET', 'POST'])
 def modify(num):
     if request.method == "POST":
-        print(type(num))
         if num == 0:
             # 新建博客
             current_blog = Blog()
@@ -62,11 +61,13 @@ def modify(num):
         # 如果获取 blog 失败，返回错误信息
         if not current_blog:
             return 404
-        current_blog.write_item(mtype="title",value=receive_data['title'])
-        current_blog.write_item(mtype="permission", value=receive_data['permission'])
-        current_blog.write_item(mtype="introduce", value=receive_data['introduce'])
-        current_blog.write_item(mtype="article", value=receive_data['article'])
-        current_blog.save_blog()
+        Flag = current_blog.write_item(mtype="title",value=receive_data['title']) & True
+        Flag &= current_blog.write_item(mtype="permission", value=receive_data['permission'])
+        Flag &= current_blog.write_item(mtype="assort", value=receive_data['assort'])
+        Flag &= current_blog.write_item(mtype="introduce", value=receive_data['introduce'])
+        Flag &= current_blog.write_item(mtype="article", value=receive_data['article'])
+        if Flag:
+            current_blog.save_blog()
         return "/blog/"+str(current_blog.get_item(mtype="id"))
     else:
         if num == 0:
@@ -108,3 +109,16 @@ def get_next_blogs():
             return_data['blogs'].append(blog)
             return_data['length'] += 1
     return jsonify(result=return_data)
+
+
+@blog.route('/get_all_assort')
+def get_all_assort():
+    uuid = request.args.get("uuid",None)
+    current_blog = Blog(uuid)
+    if uuid:
+        current_assort = current_blog.get_item(mtype="assort")
+    else:
+        current_assort = None
+    all_assort = current_blog.get_all_assort()
+    return_data = {"current_assort":current_assort,"assorts":all_assort}
+    return jsonify(return_data)
