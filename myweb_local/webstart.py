3b14c503735d2e781608984f3a1cd23ff1b5418d
huashuigou <1427963351@qq.com>
2020-01-30 21:34:35 +0800

    video类注释修改

--- myweb_local/webstart.py
+++ myweb_local/webstart.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     # 服务器使用
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
     # 本地测试使用
-    # app.run(port=8000)
+    app.run(port=8000)
